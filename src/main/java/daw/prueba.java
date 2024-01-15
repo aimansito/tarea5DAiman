4845a731bc6a8addf51db9e0c75ada17ad7236f7
Aiman <ahardao1001@g.educaand.es>
2024-01-15 09:55:17 +0100

    commit 1

---
+++ src/main/java/daw/prueba.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author aiman
+ */
+public class prueba {
+    String prueba ;
+    int numPrueba;
+}
