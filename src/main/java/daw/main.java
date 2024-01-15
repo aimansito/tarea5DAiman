583f9b0fdeae6f4c88e8c9d165ec1efab799e436
victorMenaFlores <menaflores.victor@gmail.com>
2024-01-15 10:05:06 +0100

    V1: creo main

---
+++ src/main/java/daw/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author victor
+ */
+public class main {
+    public static void main(String[] args) {
+        prueba uno = new prueba("Victor", 1);
+        prueba dos = new prueba("Daniel", 1);
+        
+        System.out.println(uno);
+        
+    }
+}
